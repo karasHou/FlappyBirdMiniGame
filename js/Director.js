c759b878e9842373b791a8e0fe1bb201f796745c
houweix <houwei19960927@126.com>
2018-02-22 22:36:26 +0800

    滚动地板

--- js/Director.js
+++ js/Director.js
@@ -8,7 +8,7 @@ export class Director {
 
     //基于ES6实现的单例模式
     static getInstance() {
-        if(!Director.instance){
+        if (!Director.instance) {
             Director.instance = new Director();
         }
         return Director.instance;
@@ -17,6 +17,13 @@ export class Director {
     run() {
         this.dataStore.get('background').draw();
         this.dataStore.get('land').draw();
+        //自适应浏览器的帧率,提高性能
+        let timer = requestAnimationFrame(() => this.run());
+        //将其存入dataStore中
+        this.dataStore.put('timer',timer);
+
+        //当游戏结束，停止动画
+        // cancelAnimationFrame(this.dataStore.get('timer'));
     }
 
 }
--- js/runtime/Land.js
+++ js/runtime/Land.js
@@ -7,16 +7,20 @@ export class Land extends Sprite {
         super(image, 0, 0,
             image.width, image.height,
             0, window.innerHeight - image.height,
-            image.width,image.height);
+            image.width, image.height);
         //地板的水平变化坐标
         this.landX = 0;
         //地板的移动速度（px）
         this.landSpeed = 2;
     }
 
-    draw(){
+    draw() {
         //每次绘制坐标加变化的坐标
-        this.landX  = this.landX + this.landSpeed;
+        this.landX = this.landX + this.landSpeed;
+        //判断边界
+        if (this.landX > (this.img.width - window.innerWidth)) {
+            this.landX = 0;
+        }
         super.draw(this.img,
             this.srcX,
             this.srcY,
